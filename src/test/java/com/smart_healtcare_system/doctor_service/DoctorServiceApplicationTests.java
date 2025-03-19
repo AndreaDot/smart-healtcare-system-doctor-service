65c472c0890f066fc18dfde1cb2fcb853c5f3f4f
AndreaDottavio <andrea.dottavio_94@yahoo.it>
2025-03-19 11:15:54 +0100

    Doctor Service first commit

---
+++ src/test/java/com/smart_healtcare_system/doctor_service/DoctorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_healtcare_system.doctor_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
